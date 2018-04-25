a6a7443f0c52882d3e1f07cfe3e6f38827f4a5ef
Liz Chaddock <elizabeth.chaddock@ubtrobot.com>
2018-04-24 18:14:01 -0700

    Initial app

---
+++ Todo/TodoListViewController.swift
@@ -0,0 +1,41 @@
+//
+//  TodoListViewController.swift
+//  Todo
+//
+//  Created by Liz Chaddock on 4/24/18.
+//  Copyright © 2018 Liz Chaddock. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController : UITableViewController {
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //MARK - TableView Data Source methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    //MARK - TableView Delegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+            
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
